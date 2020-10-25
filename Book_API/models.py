9dfd0639afc549c137e5491e5b55fad96b4eb3f0
Ravindrakumara <ravindrakumara07@hotmail.com>
2020-10-26 00:53:22 +0530

    make it better

--- Book_API/models.py
+++ Book_API/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django_dropbox.storage import DropboxStorage
 
-STORAGE = DropboxStorage()
+DROPBOX_STORAGE = DropboxStorage()
 # Create your models here.
 
 def upload_path(instance, filname):
@@ -32,7 +32,7 @@ class Books (models.Model):
     pages = models.CharField(max_length=50)
     note = models.CharField(max_length=65)
     edition = models.CharField(max_length=50)
-    image = models.ImageField(upload_to='book_cover',storage=STORAGE, null=True, blank=True)
+    image = models.ImageField(upload_to='book_cover',storage=DROPBOX_STORAGE, null=True, blank=True)
     create_date = models.DateTimeField(auto_now_add=True)
     class Meta:
         verbose_name_plural = "Books"
@@ -79,7 +79,7 @@ class Carousel(models.Model):
 
 
     headline = models.CharField(max_length=65)
-    banner = models.ImageField(upload_to='web_banner',storage=STORAGE, null=True, blank=True)
+    banner = models.ImageField(upload_to='web_banner',storage=storage=DROPBOX_STORAGE, null=True, blank=True)
     create_date = models.DateTimeField(auto_now_add=True)
 
     #class Meta:
